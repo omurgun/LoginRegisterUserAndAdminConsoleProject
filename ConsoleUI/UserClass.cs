24fa347f70215f09be1000738232a04460fc9ddd
Ömür Gün <omur_gun@outlook.com>
2020-11-19 15:43:06 +0300

    user class commit

--- ConsoleUI/UserClass.cs
+++ ConsoleUI/UserClass.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.DependencyResolvers.Ninject;
+using Entities.Concrete;
 
 namespace ConsoleUI
 {
     public class UserClass
     {
+        private readonly IUserService _userService = InstanceFactory.GetInstance<IUserService>();
         public void UserMenu()
         {
             Console.WriteLine("\t\t\tWelcome User Panel");
-            Console.WriteLine("1- Add User");
-            Console.WriteLine("2- Update User");
-            Console.WriteLine("3- Delete User");
-            Console.WriteLine("4- Get User");
-            Console.WriteLine("5- GetAll User");
+            Console.WriteLine("1- Login");
+            Console.WriteLine("2- Register");
             Console.WriteLine("0- Exit");
 
-            Console.Write("please choose (0-5): ");
+            Console.Write("please choose (0-2): ");
             var choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
@@ -28,28 +29,32 @@ namespace ConsoleUI
                     break;
                 case 1:
                     Console.Clear();
-                    
+                    Login();
                     break;
                 case 2:
                     Console.Clear();
-                   
-                    break;
-                case 3:
-                    Console.Clear();
-                   
-                    break;
-                case 4:
-                    Console.Clear();
-                   
-                    break;
-                case 5:
-                    Console.Clear();
-                   
+                    Register();
                     break;
                 default:
                     Console.WriteLine("Invalid choice!");
                     break;
             }
+        }
+
+        private void Login()
+        {
+            Console.WriteLine("\t\t\tLogin");
+            Console.Write("Username : ");
+            var userName = Console.ReadLine();
+            Console.Write("Password : ");
+            var password = Console.ReadLine();
+
+
+        }
+
+        private void Register()
+        {
+
         }
     }
 }
